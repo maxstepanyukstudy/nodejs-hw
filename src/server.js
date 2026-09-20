dcbb8c6de9146f68bd8086fc4a926672f5839567
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-09-20 19:43:17 +0300

    use cors

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
 app.get("/notes", (req, res) => {
   res.status(200).json({
