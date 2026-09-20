d553431fd467688851330ac42ae01efaf3f75f06
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-09-20 19:41:05 +0300

    add express setup

--- src/server.js
+++ src/server.js
@@ -1,3 +1,9 @@
 import "dotenv/config";
+import express from "express";
+
+const app = express();
 
 const PORT = Number(process.env.PORT) || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
