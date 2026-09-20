5f8da2d17428fe8f18b42d14e9f92ab6707380e4
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-09-20 19:55:13 +0300

    app.use(pino({...}))

--- src/server.js
+++ src/server.js
@@ -1,11 +1,28 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import pino from "pino-http";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(
+  pino({
+    level: "info",
+    transport: {
+      target: "pino-pretty",
+      options: {
+        colorize: true,
+        translateTime: "yyyy-MM-dd HH:MM:ss",
+        ignore: "pid",
+        hideObject: true,
+        messageFormat:
+          "{req.method} {req.url} {res.statusCode} - {responseTime}ms",
+      },
+    },
+  }),
+);
 
 app.get("/notes", (req, res) => {
   res.status(200).json({
